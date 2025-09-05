674ef8fa39d05502226ad48c01877e651bedb49a
DJ BlackEagle <122323412+DJBlackEagle@users.noreply.github.com>
2025-09-05 18:26:25 +0200

    feat(frontend): update SafeHtmlPipe to sanitize HTML content and handle security breaches

--- packages/apps/frontend/src/app/pipes/safe-html.pipe.ts
+++ packages/apps/frontend/src/app/pipes/safe-html.pipe.ts
@@ -1,14 +1,16 @@
-import { Pipe } from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { Pipe, PipeTransform, SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Pipe({
   name: 'safeHtml',
   standalone: true,
 })
-export class SafeHtmlPipe {
+export class SafeHtmlPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
 
-  transform(html: string): SafeHtml {
-    return this.sanitizer.bypassSecurityTrustHtml(html);
+  transform(html: string): string {
+    const result = this.sanitizer.sanitize(SecurityContext.HTML, html);
+
+    return result ? result : 'SECURITY BREACH';
   }
 }
