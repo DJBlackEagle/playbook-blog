cddb5cfa800482fe00e0ed807d8b4a317817881f
DJ BlackEagle <122323412+DJBlackEagle@users.noreply.github.com>
2025-09-06 16:40:46 +0200

    feat(confirmation-dialog): enhance dialog accessibility by focusing on cancel button

--- packages/apps/frontend/src/app/components/confirmation-dialog/confirmation-dialog.component.ts
+++ packages/apps/frontend/src/app/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  ElementRef,
+  inject,
+  ViewChild,
+} from '@angular/core';
 import { ConfirmationDialogService } from '../../services/confirmation-dialog.service';
 
 @Component({
@@ -11,4 +18,16 @@ import { ConfirmationDialogService } from '../../services/confirmation-dialog.se
 })
 export class ConfirmationDialogComponent {
   readonly dialogService = inject(ConfirmationDialogService);
+
+  @ViewChild('cancelButton') cancelButton?: ElementRef<HTMLButtonElement>;
+
+  constructor() {
+    effect(() => {
+      if (this.dialogService.state().visible) {
+        setTimeout(() => {
+          this.cancelButton?.nativeElement.focus();
+        }, 0);
+      }
+    });
+  }
 }
