11b1d7096c7dcc0261e0c3ac7f62effcefedf2d8
DJ BlackEagle <122323412+DJBlackEagle@users.noreply.github.com>
2025-11-26 17:31:53 +0100

    g

--- packages/apps/frontend/src/app/features/storybook/storybook-container/storybook-container.ts
+++ packages/apps/frontend/src/app/features/storybook/storybook-container/storybook-container.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Button } from '../../../shared/components/button/button';
 import { CardProgress } from '../../../shared/components/card-progress/card-progress';
+import { CardProgressList } from '../../../shared/components/card-progress/card-progress-list';
 import { Container } from '../../../shared/components/container/container';
 import { ContainerItem } from '../../../shared/components/container/container-item.model';
 import { ContainerList } from '../../../shared/components/container/container-list';
@@ -14,24 +15,37 @@ import { ContainerList } from '../../../shared/components/container/container-li
 export class StorybookContainer {
   container01: ContainerItem;
   container02: ContainerItem;
-  container02_01: ContainerItem;
-  container02_02: ContainerItem;
   containers: ContainerItem[] = [];
 
   constructor() {
+    this.container01 = {
+      title: 'Container Component',
+      subTitle: 'This is a subtitle for the container component.',
+      content:
+        'This is the content of the container component. It can include text, images, or other components as needed.',
+    };
+
+    // const x = new CardProgress();
+    // x.cardProgressItem = { title: 'Progress Example', precentage: 75 };
+
     // this.container01 = {
-    //   title: 'Container Component',
-    //   subTitle: 'This is a subtitle for the container component.',
-    //   content:
-    //     'This is the content of the container component. It can include text, images, or other components as needed.',
+    //   title: 'First Container',
+    //   subTitle: 'Subtitle for the first container.',
+    //   content: {
+    //     component: CardProgress,
+    //     inputs: {
+    //       cardProgressItem: {
+    //         title: '📢 Single Card Title #1 (Single)',
+    //         content: '📄 This is a description for Card 1.',
+    //         precentage: 75,
+    //       },
+    //     },
+    //   },
     // };
 
-    const x = new CardProgress();
-    x.cardProgressItem = { title: 'Progress Example', precentage: 75 };
-
-    this.container01 = {
-      title: 'First Container',
-      subTitle: 'Subtitle for the first container.',
+    this.container02 = {
+      title: 'Second Container',
+      subTitle: 'Subtitle for the second container.',
       content: {
         component: CardProgress,
         inputs: {
@@ -44,27 +58,6 @@ export class StorybookContainer {
       },
     };
 
-    this.container02 = {
-      title: 'Second Container',
-      subTitle: 'Subtitle for the second container.',
-      content:
-        'Content for the second container component. You can customize this content as required.',
-    };
-
-    this.container02_01 = {
-      title: 'Nested Container 1',
-      subTitle: 'Subtitle for nested container 1.',
-      content:
-        'Content for the first nested container component. This demonstrates nesting of containers.',
-    };
-
-    this.container02_02 = {
-      title: 'Nested Container 2',
-      subTitle: 'Subtitle for nested container 2.',
-      content:
-        'Content for the second nested container component. Nesting helps in organizing content hierarchically.',
-    };
-
     this.containers = [
       {
         title: 'Another Container',
@@ -73,10 +66,36 @@ export class StorybookContainer {
           'Content for the second container component. You can add as many containers as needed.',
       },
       {
+        title: 'Container with CardProgressList',
+        subTitle: 'Subtitle for container with button.',
+        content: {
+          component: CardProgressList,
+          inputs: {
+            cardProgressItems: [
+              { title: '📢 Card Title #1', precentage: 20 },
+              { title: '📢 Card Title #2', precentage: 40 },
+            ],
+          },
+        },
+      },
+      {
         title: 'Third Container',
         subTitle: 'Subtitle for the third container.',
         content:
           'Content for the third container component. This demonstrates multiple container usage.',
+      },
+      {
+        title: 'Container with CardProgressList',
+        subTitle: 'Subtitle for container with button.',
+        content: {
+          component: CardProgress,
+          inputs: {
+            cardProgressItem: {
+              title: '📢 Single Card Title #2 (With Button)',
+              content: '📄 This is a description for Card 2.',
+            },
+          },
+        },
       },
       {
         title: 'Fourth Container',
